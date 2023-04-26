b73c560245c2e4b6c4fb8b73087e801f2f862e28
davidhartsough <hartsoughdavid@gmail.com>
2023-04-26 00:22:20 -0600

    add the app dir and serve it

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
-// const http = require("http");
-// const fs = require("fs");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const { PeerServer } = require("peer");
 
 // const peerServer =
 PeerServer({ port: 9000, path: "/p", key: "peerjs" });
 
-// http
-//   .createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader("Content-Type", "text/html");
-//     fs.createReadStream(filepath).pipe(res);
-//   })
-//   .listen(3000);
+http
+  .createServer((req, res) => {
+    if (req.method === "GET") {
+      console.log("req.url:", req.url);
+      if (req.url === "/") {
+        fs.readFile("./app/index.html", (err, data) => {
+          if (!err) {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.write(data);
+            res.end();
+          } else {
+            res.statusCode = 404;
+            res.setHeader("Content-Type", "text/plain");
+            res.write("Error 404: Not Found");
+            res.end();
+          }
+        });
+      } else if (req.url === "/app.js") {
+        fs.readFile("./app/app.js", (err, data) => {
+          if (!err) {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/javascript");
+            res.write(data);
+            res.end();
+          } else {
+            res.statusCode = 404;
+            res.setHeader("Content-Type", "text/plain");
+            res.write("Error 404: Not Found");
+            res.end();
+          }
+        });
+      } else if (req.url === "/favicon.ico") {
+        fs.readFile("./app/favicon.ico", (err, data) => {
+          if (!err) {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "image/x-icon");
+            res.write(data);
+            res.end();
+          } else {
+            res.statusCode = 404;
+            res.setHeader("Content-Type", "text/plain");
+            res.write("Error 404: Not Found");
+            res.end();
+          }
+        });
+      } else {
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "text/plain");
+        res.write("Error 404: Not Found");
+        res.end();
+      }
+    }
+  })
+  .listen(5000);
