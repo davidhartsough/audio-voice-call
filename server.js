40d6a9f579995522f489cc7b3fe5c4bc2a85a100
davidhartsough <hartsoughdavid@gmail.com>
2023-04-25 23:17:12 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// const http = require("http");
+// const fs = require("fs");
+const { PeerServer } = require("peer");
+
+// const peerServer =
+PeerServer({ port: 9000, path: "/p", key: "peerjs" });
+
+// http
+//   .createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "text/html");
+//   })
+//   .listen(3000);
